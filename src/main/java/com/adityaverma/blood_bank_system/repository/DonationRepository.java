b84a4ed882458061fdf285b5f34c4754cdec645d
AdityaVerma <adityaverma8392@gmail.com>
2026-01-09 16:50:48 +0530

    DonationReposiotry.java Added

--- src/main/java/com/adityaverma/blood_bank_system/repository/DonationRepository.java
+++ src/main/java/com/adityaverma/blood_bank_system/repository/DonationRepository.java
@@ -1,4 +1,25 @@
 package com.adityaverma.blood_bank_system.repository;
 
-public class DonationRepository {
+import com.adityaverma.blood_bank_system.model.Donation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface DonationRepository extends MongoRepository<Donation, String> {
+
+    List<Donation> findByDonorId(String donorId);
+    List<Donation> findByBloodBankId(String bloodBankId);
+
+    @Query("{'donorId': ?0, 'donationDate': {$gte: ?1}}")
+    List<Donation> findRecentDonations(String donorId, LocalDateTime date);
+
+    @Query(value = "{'donorId': ?0}", count = true)
+    long countDonationsByDonor(String donorId);
+
+    @Query("{'donationDate': {$gte: ?0, $lte: ?1}}")
+    List<Donation> findByDateRange(LocalDateTime start, LocalDateTime end);
 }
