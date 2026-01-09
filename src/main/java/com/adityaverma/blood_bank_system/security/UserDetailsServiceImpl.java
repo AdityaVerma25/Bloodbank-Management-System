47732933721183bf4ce77c5849e8f28474fe89cd
AdityaVerma <adityaverma8392@gmail.com>
2026-01-09 17:18:36 +0530

    User details service imlp added

--- src/main/java/com/adityaverma/blood_bank_system/security/UserDetailsServiceImpl.java
+++ src/main/java/com/adityaverma/blood_bank_system/security/UserDetailsServiceImpl.java
@@ -1,4 +1,34 @@
 package com.adityaverma.blood_bank_system.security;
 
-public class UserDetailsServiceImpl {
+import com.adityaverma.blood_bank_system.model.User;
+import com.adityaverma.blood_bank_system.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findActiveByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return UserDetailsImpl.build(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(String id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with id: " + id));
+
+        return UserDetailsImpl.build(user);
+    }
 }
