7dbd6ee0e74b4966beef01b95e0731c55682d12c
AdityaVerma <adityaverma8392@gmail.com>
2026-01-09 16:43:46 +0530

    Blood Component Class Added

--- src/main/java/com/adityaverma/blood_bank_system/model/BloodComponent.java
+++ src/main/java/com/adityaverma/blood_bank_system/model/BloodComponent.java
@@ -1,4 +1,31 @@
 package com.adityaverma.blood_bank_system.model;
 
-public class BloodComponent {
+public enum BloodComponent {
+    WHOLE_BLOOD("Whole Blood", 35, "1-6°C"),
+    PLASMA("Plasma", 365, "-25°C or below"),
+    PLATELETS("Platelets", 5, "20-24°C"),
+    RED_BLOOD_CELLS("Red Blood Cells", 42, "1-6°C"),
+    CRYOPRECIPITATE("Cryoprecipitate", 365, "-25°C or below");
+
+    private final String displayName;
+    private final int shelfLifeDays;
+    private final String storageTemperature;
+
+    BloodComponent(String displayName, int shelfLifeDays, String storageTemperature) {
+        this.displayName = displayName;
+        this.shelfLifeDays = shelfLifeDays;
+        this.storageTemperature = storageTemperature;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public int getShelfLifeDays() {
+        return shelfLifeDays;
+    }
+
+    public String getStorageTemperature() {
+        return storageTemperature;
+    }
 }
